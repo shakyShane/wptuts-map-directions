2d3db8dfe14bc9cbe52afdad918737df7ffa51fc
Shane Osbourne <shane.osbourne8@gmail.com>
2012-07-10 18:47:13 +0100

    Add nicer error messages

--- map/map.js
+++ map/map.js
@@ -15,7 +15,13 @@
 
         showDirections:function (dirResult, dirStatus) {
             if (dirStatus != google.maps.DirectionsStatus.OK) {
-                alert('Directions failed: ' + dirStatus);
+                switch (dirStatus){
+                    case "ZERO_RESULTS" : alert ('Sorry, we can\'t provide directions to that address. Please try again.')
+                        break;
+                    case "NOT_FOUND" : alert('Sorry we didn\'t understand the address you entered - Please try again.');
+                        break;
+                    default : alert('Sorry, there was a problem generating the directions. Please try again.')
+                }
                 return;
             }
             // Show directions
